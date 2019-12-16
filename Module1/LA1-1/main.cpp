28731a100849daf2e6c9f6c04a801705b8e31677
Brandon Burke <brandonburke@ad.weber.edu>
2019-12-16 09:53:12 -0700

    Add input

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 int main() 
 {
+    int age;  //int: signed integers
+
+    // cout: for system output
     std::cout << "This is a test program " << std::endl;
+    cout << "Another line under namespace std " << endl;
+    cout << "Wildcats" << "WSU" << "Ogden" << endl;
+
+    //cin: for system/user input
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "I see you are: " << age << endl;
     return 0;
 }

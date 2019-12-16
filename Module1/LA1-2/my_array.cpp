cba3a6fcdc87c6b60c063b86c6701cff8e1913d2
Brandon Burke <brandonburke93@outlook.com>
2019-12-16 15:10:40 -0700

    Add my_array.cpp

--- Module1/LA1-2/my_array.cpp
+++ Module1/LA1-2/my_array.cpp
@@ -19,14 +19,36 @@ int main()
     // Array notation: type name[elements];
     int grades[kMaxNum];  // an array of 5 integers
     int sum = 0;
+    float average = 0;
     cout << "Enter your exam grades" << endl;
     for(int num = 0; num < kMaxNum; ++num)
     {
         cout << "Enter " << num + 1 << " grade: " << endl;
         cin >> grades[num];
         sum += grades[num];
+        average += grades[num];
     }
-    float average = sum/kMaxNum;
-    cout << "The average of entered grades is: " << average;
+    average = average/kMaxNum;
+    cout << "The average of entered grades is: " << average << endl;
+    average = sum/kMaxNum;
+    cout << "The average of entered grades is: " << average << endl;
+
+    // 2D Arrays: type name[row][col]
+    int bi_array[3][5] = {
+        {2, 4, 6, 11, 67},
+        {4, 5, 22, 33, 99},
+        {89, 33, 22, 11, 0}
+    };
+
+    // Print each element in the array
+    for(int row = 0; row < 3; ++row)
+    {
+        for(int column = 0; column < 5; ++ column)
+        {
+            cout << "Array element at [" << row << "]" <<
+                    "[" << column << "] = " << bi_array[row][column] << endl;
+        }
+    }    
+
     return 0;
 }

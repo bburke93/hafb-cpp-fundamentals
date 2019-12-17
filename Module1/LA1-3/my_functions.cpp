fe60d428b5d350e3b09a5559552c0c92e0c14c56
Brandon Burke <brandonburke@ad.weber.edu>
2019-12-17 12:00:02 -0700

    Challenge

--- Module1/LA1-3/my_functions.cpp
+++ Module1/LA1-3/my_functions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
 /**
@@ -32,4 +33,55 @@ void UpdateStep(int& step) // Take the address of the parameter
     cout << "Current step: " << step << endl;
     ++step;
     cout << "New step: " << step << endl;
+}
+
+/**
+ * @brief Return the higher of two given integers
+ * 
+ * @param num1: First value 
+ * @param num2: Second value
+ * @return int: The higher of the two values
+ */
+int Max(int num1, int num2)
+{
+  if (num1 > num2)
+  {
+    return num1;
+  }
+  else
+  {
+    return num2;
+  }
+  
+}
+
+int Max3(int num1, int num2, int num3)
+{
+  // num1 greatest
+  if((num1 > num2) && (num1 > num3))
+  {
+    return num1;
+  }
+  // num2 greatest
+  else if((num2 > num1) && (num2 > num3))
+  {
+    return num2;
+  }
+  else //num3 greatest
+  {
+    return num3;
+  }
+}
+
+int Max10(std::array<int, 10> nums)
+{
+  int max = 0;
+  for(auto num : nums)
+  {
+    if (max <= num)
+    {
+      max = num;
+    }
+  }
+  return max;
 }
--- Module1/LA1-3/my_functions.h
+++ Module1/LA1-3/my_functions.h
@@ -1,11 +1,8 @@
-/*
-** EPITECH PROJECT, 2019
-** hafb-cpp-fundamentals
-** File description:
-** my_functions
-*/
-
+#include <array>
 #pragma once
 
 int Sum(int num1, int num2);
 int UpdateStep(int& step);
+int Max(int num1, int num2);
+int Max3(int num1, int num2, int num3);
+int Max10(std::array<int, 10> nums);

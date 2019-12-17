7c1a3236ac77b77e2bdd19220ed34e49c9e06f22
Brandon Burke <brandonburke@ad.weber.edu>
2019-12-17 14:09:33 -0700

    Finish Mod 1

--- Module1/TO1-5/src/cars.cpp
+++ Module1/TO1-5/src/cars.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 #include "cars.h"
+
+int HorsePower()
+{
+    return 150;
+}
+
 // using namespace lamborghini;
+int lamborghini::HorsePower() {
+    return 760;
+}
+
+void lamborghini::cout() {
+    std::cout << "Lamborghini makes the best tractors";
+}
+
 // using namespace porsche;
+int porsche::HorsePower()
+{
+    return 700;
+}
--- Module1/TO1-5/src/main.cpp
+++ Module1/TO1-5/src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-
-#include <iostream>
 #include "cars.h"
-// using namespace std;
+//using namespace std;
 // using namespace lamborghini;
 // using porsche::HorsePower;
 
@@ -11,6 +9,8 @@ int main()
 {
     std::cout << "The lamborghini's horse power: " << lamborghini::HorsePower() << std::endl;
     std::cout << "The porsche's horse power: " << porsche::HorsePower() << std::endl;
-    std::cout << "A regular car's horse power: " << HorsePower() << std::endl;
+    // :: the base nameless namespace
+    std::cout << "A regular car's horse power: " << ::HorsePower() << std::endl;
+    lamborghini::cout();
     return 0;
 }

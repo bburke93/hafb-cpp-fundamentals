7ada23d93247693249fced5635ce911647b635c9
Brandon Burke <brandonburke93@outlook.com>
2019-12-18 16:12:24 -0700

    Operator overloading

--- Module3/LA3-1/src/counter.h
+++ Module3/LA3-1/src/counter.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <iostream>
+
+class Counter
+{
+    private:
+        unsigned int count_;
+    public:
+        Counter() : count_(0) {}
+        Counter(unsigned int count) : count_(count) {}
+        ~Counter() {std::cout << "Lights fading\n";}
+        // Setter & getter
+        unsigned int count() {return count_;}
+        void set_count(unsigned int count) {count_ = count;}
+        // Operator overloads
+        // void operator ++() {++count_;}
+        Counter operator ++();
+        Counter operator ++(int);
+};
+

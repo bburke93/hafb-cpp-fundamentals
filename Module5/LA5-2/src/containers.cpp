e025e8b198a376380494081db2172b08ca374c9d
Brandon Burke <brandonburke93@outlook.com>
2019-12-19 16:08:36 -0700

    Sets and maps

--- Module5/LA5-2/src/containers.cpp
+++ Module5/LA5-2/src/containers.cpp
@@ -3,3 +3,20 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+
+void Load_Data(std::vector<Data> &data, const std::string &input_file)
+{
+    csvstream csvinput(input_file);  
+    // Rows have a key = column name, value = cell data
+    std::map<std::string, std::string> row;
+    // Extract the animal column
+    Data temp;
+    while(csvinput >> row)
+    {
+        // The key is the column name
+        temp.id = std::stoi(row["id"]); 
+        temp.name = row["name"];
+        temp.animal = row["animal"];
+        data.push_back(temp);
+    }
+}
--- Module5/LA5-2/src/main.cpp
+++ Module5/LA5-2/src/main.cpp
@@ -3,12 +3,29 @@
 #include <iostream>
 #include <string>
 #include <map>
-using namespace std;
+// using namespace std;
 
 // Main Function
 int main()
 {
-  
+  // csvstream csvinput("../data/input.csv");  
+  // // Rows have a key = column name, value = cell data
+  // std::map<std::string, std::string> row;
+  // // Extract the animal column
+  // while(csvinput >> row)
+  // {
+  //   // The key is the column name
+  //   std::cout << row["animal"] << std::endl;
+  // }
+
+  std::vector<Data> pet_owners;
+  std::string in_file = "../data/input.csv";
+  Load_Data(pet_owners, in_file);
+  for(const auto& owner : pet_owners)
+  {
+    std::cout << owner.id << "\t" << owner.name << "\t" 
+      << owner.animal << std::endl;
+  }
 
   return 0;
 }

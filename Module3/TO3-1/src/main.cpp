9edb256bb831e46ba970b7172f84ceb39661cfaa
Brandon Burke <brandonburke93@outlook.com>
2019-12-19 09:55:20 -0700

    TO3-1 complete

--- Module3/TO3-1/src/main.cpp
+++ Module3/TO3-1/src/main.cpp
@@ -5,5 +5,21 @@ using namespace std;
 // Main Function
 int main() 
 {
+  Distance d1 = Distance(1, 6);
+  d1.ShowDist();
+
+  Distance d2(0, 12);
+  Distance d3;
+
+  // add two objects
+  d3 = d1 + d2;
+
+  // Display info with std::cout
+  std::cout << d3 << std::endl;
+
+  // subtract two objects
+  Distance d4 = d1 - d2;
+  std::cout << d4 << std::endl;
+  
   return 0;
 }
